c87e21c97be54181d5bbf4f4046aae441eecbdb9
lexussab <lexussab@gmail.com>
2014-04-14 14:46:13 +0400

    обновлен метод compare для учета времени регистрации

--- MyTest/src/main/resources/java/ru/smirnov/anton/CompareCPU.java
+++ MyTest/src/main/resources/java/ru/smirnov/anton/CompareCPU.java
@@ -7,11 +7,14 @@ public class CompareCPU implements Comparator<Record>{
 	public int compare(Record rec1, Record rec2){
 		int cpu1 = rec1.getCpu();
 		int cpu2 = rec2.getCpu();
+		long d1 = rec1.getDateReg().getTime();
+		long d2 = rec2.getDateReg().getTime();
 		
 		if (cpu1 > cpu2) return 1;
 		else if (cpu1 < cpu2) return -1;
-		else if (cpu1 == cpu2)
-			return 0;
+		else if (cpu1 == cpu2 && d1 < d2) return -1;
+		else if (cpu1 == cpu2 && d1 > d2) return 1;
+		
 		return cpu2;
     }
 }
